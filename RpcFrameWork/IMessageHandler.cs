04a809c7db6f494f502daf5657ca264787a3aa00
Yowe <ruwangxiansheng@sina.com>
2017-09-22 17:15:40 +0800

    add messagehandler

---
+++ RpcFrameWork/IMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork
+{
+
+    public delegate void MessageHandlingEvent(BasicDeliverEventArgs eventArgs);
+
+    public delegate void MessageWasNotHandledEvent(BasicDeliverEventArgs eventArgs);
+
+    public interface IMessageHandler
+    {
+        void HandleMessage(BasicDeliverEventArgs eventArg);
+        void HandleError(BasicDeliverEventArgs eventArg, Exception excepton);
+        /// <summary>
+        /// 消息处理完毕回调
+        /// </summary>
+        event MessageHandlingEvent HandlingComplete;
+        /// <summary>
+        /// 消息处理失败后回调
+        /// </summary>
+        event MessageWasNotHandledEvent MessageWasNotHandled;
+    }
+}
