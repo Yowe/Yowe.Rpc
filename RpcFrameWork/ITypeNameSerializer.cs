bef1da2935f2f5a1d1362fba623373aa6a16daf0
Yowe <ruwangxiansheng@sina.com>
2017-09-27 19:06:39 +0800

    add typeserializer

---
+++ RpcFrameWork/ITypeNameSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork
+{
+    public interface ITypeNameSerializer
+    {
+        string Serialize(Type type);
+    }
+}
---
+++ RpcFrameWork/Internal/TypeNameSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork.Internal
+{
+    public class TypeNameSerializer : ITypeNameSerializer
+    {
+        public string Serialize(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return type.FullName.Replace('.', '_')
+                + ":"
+                + type.Assembly.GetName().Name.Replace('.', '_');
+        }
+    }
+}
