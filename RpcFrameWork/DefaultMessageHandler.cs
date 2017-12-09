fef72c076665a7c1498511136a7ed1fea8857cd2
Yowe <ruwangxiansheng@sina.com>
2017-12-09 23:39:34 +0800

    新增消息处理类

---
+++ RpcFrameWork/DefaultMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork.Internal
+{
+    public class DefaultMessageHandler<T>:IMessageHandler
+    {
+        protected readonly string _typeName = Global.DefaultTypeNameSerializer.Serialize(typeof(T));
+        protected readonly string _subscriptionName;
+        protected readonly IRabbitWatcher _watcher;
+        protected readonly Action<T, MessageDeliverEventArgs> _msgHandlingAction;
+        protected readonly IConsumerErrorHandler _consumerErrorHandler;
+        protected readonly ISerializer _messageSerializer;
+
+        public event MessageHandlingEvent HandlingComplete;
+        public event MessageWasNotHandledEvent MessageWasNotHandled;
+
+        public DefaultMessageHandler(string subscriptionName,
+            Action<T, MessageDeliverEventArgs> msgHandlingAction,
+            IConsumerErrorHandler consumerErrorHandler,
+            ISerializer messageSerializer,
+            IRabbitWatcher watcher)
+        {
+            if (msgHandlingAction == null)
+                throw new ArgumentNullException(nameof(msgHandlingAction));
+
+            if (consumerErrorHandler == null)
+                throw new ArgumentNullException(nameof(consumerErrorHandler));
+
+            if (messageSerializer == null)
+                throw new ArgumentNullException(nameof(messageSerializer));
+
+            if (watcher == null)
+                throw new ArgumentNullException(nameof(watcher));
+
+            _subscriptionName = subscriptionName;
+            _watcher = watcher;
+            _consumerErrorHandler = consumerErrorHandler;
+            _messageSerializer = messageSerializer;
+            _msgHandlingAction = msgHandlingAction;
+            _typeName = Global.DefaultTypeNameSerializer.Serialize(typeof(T));
+        }
+
+    }
+}
---
+++ RpcFrameWork/MessageDeliverEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork
+{
+    public class MessageDeliverEventArgs
+    {
+        public string SubscriptionName
+        {
+            get;set;
+        }
+
+        public string CounsumerTag
+        {
+            get;set;
+        }
+
+        public string DeliveryTag
+        {
+            get; set;
+        }
+
+        public string MessagePriorty
+        {
+            get;set;
+        }
+    }
+}
