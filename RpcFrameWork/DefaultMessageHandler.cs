3e4cd34f8c90758634d7af7bf4252c71cb63f749
Yowe <ruwangxiansheng@sina.com>
2017-12-10 22:26:35 +0800

    完善异常处理

--- RpcFrameWork/DefaultMessageHandler.cs
+++ RpcFrameWork/DefaultMessageHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,6 +42,60 @@ namespace RpcFrameWork.Internal
             _messageSerializer = messageSerializer;
             _msgHandlingAction = msgHandlingAction;
             _typeName = Global.DefaultTypeNameSerializer.Serialize(typeof(T));
+        }
+        /// <summary>
+        /// 在处理信息之前可以重新此方法
+        /// </summary>
+        /// <param name="args"></param>
+        protected virtual void BeforeHandingMessage(BasicDeliverEventArgs args)
+        {
+        }
+
+        /// <summary>
+        /// 处理信息之后可以重新此方法
+        /// </summary>
+        /// <param name="args"></param>
+        protected virtual void AfterHandngMessage(BasicDeliverEventArgs args)
+        {
+        }
+
+        public virtual void HandleError(BasicDeliverEventArgs eventArgs, Exception exception)
+        {
+            _watcher.ErrorFormat(BuildErrorLogMessage(eventArgs, exception));
+            _consumerErrorHandler.HandleError(eventArgs, exception);
+        }
+
+        protected virtual string BuildErrorLogMessage(BasicDeliverEventArgs basicDeliverEventArgs, Exception exception)
+        {
+            var message = Encoding.UTF8.GetString(basicDeliverEventArgs.Body);
+            var properties = basicDeliverEventArgs.BasicProperties as RabbitMQ.Client.Impl.BasicProperties;
+            var propertiesMessage = new StringBuilder();
+            if (properties != null)
+                properties.AppendPropertyDebugStringTo(propertiesMessage);
+
+            return "订阅方法引起异常.\n" +
+                  $"\tExchange:    '{basicDeliverEventArgs.Exchange}'\n" +
+                  $"\tRouting Key: '{basicDeliverEventArgs.RoutingKey}'\n" +
+                  $"\tRedelivered: '{basicDeliverEventArgs.Redelivered}'\n" +
+                  $" Message:\n{message}\n" +
+                  $" BasicProperties:\n{propertiesMessage}\n" +
+                  $" Exception:\n{exception}\n";
+        }
+
+        internal void CleanUp(BasicDeliverEventArgs eventArgs, bool msgHandled)
+        {
+            if (!msgHandled && MessageWasNotHandled != null)
+            {
+                try
+                {
+                    MessageWasNotHandled(eventArgs);
+                }
+                catch (Exception exceptionWhenFiringMessageWasNotDeliveredEvent)
+                {
+                    _watcher.ErrorFormat("触发MessageWasNotHandled事件时出现异常");
+                    _watcher.Error(exceptionWhenFiringMessageWasNotDeliveredEvent);
+                }
+            }
         }
 
     }
