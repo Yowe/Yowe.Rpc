e4c7bc3c5df9a7d881061ae3117f442b7f2ef508
Yowe <ruwangxiansheng@sina.com>
2017-12-14 15:55:13 +0800

    add consumerstatus

---
+++ RpcFrameWork/ConsumerStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork
+{
+    /// <summary>
+    /// 消费者状态
+    /// </summary>
+    public enum ConsumerStatus
+    {
+        /// <summary>
+        /// Active
+        /// </summary>
+        Active,
+        /// <summary>
+        /// Waiting for running tasks to finish 
+        /// </summary>
+        Disposing,
+        /// <summary>
+        /// Disposed
+        /// </summary>
+        Disposed
+    }
+}
