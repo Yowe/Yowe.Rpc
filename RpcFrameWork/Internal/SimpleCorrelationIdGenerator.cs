26299ee764acf596af054fb2ea805d12aae596ee
Yowe <ruwangxiansheng@sina.com>
2017-12-14 15:51:56 +0800

    新增GUID辅助类

---
+++ RpcFrameWork/Internal/SimpleCorrelationIdGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcFrameWork.Internal
+{
+    internal class SimpleCorrelationIdGenerator : ICorrelationIdGenerator
+    {
+        public string GenerateCorrelationId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
